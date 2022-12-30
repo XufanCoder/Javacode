ff9dfb4afb48d70581af890797774f0f28d093f6
xu-fan-fan <fanxu3424@163.com>
2022-12-30 11:58:53 +0800

    测试提交1

---
+++ Demo221230/src/Test_1.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Snk
+ * Date: 2022-12-30
+ * Time: 11:55
+ */
+public class Test_1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+
+    }
+}
