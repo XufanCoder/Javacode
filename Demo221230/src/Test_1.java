3e9f13142352505828e43e9b251d70db9f01443c
xu-fan-fan <fanxu3424@163.com>
2022-12-31 22:32:31 +0800

    链表增删查改

--- Demo221230/src/Test_1.java
+++ Demo221230/src/Test_1.java
@@ -7,8 +7,32 @@
  */
 public class Test_1 {
     public static void main(String[] args) {
-        System.out.println("hello");
-
-
+        MyList myList = new MyList();
+        myList.createLink();
+        //myList.display();
+        System.out.println(myList.contains(30));
+        System.out.println(myList.size());
+        System.out.println();
+        System.out.println("------------");
+        myList.addFirst(11);
+        myList.addFirst(22);
+        myList.addFirst(33);
+        myList.display();
+        System.out.println();
+        myList.addLast(33);
+        myList.addLast(44);
+        myList.display();
+        System.out.println();
+        System.out.println("------------");
+        myList.addIndex(66,3);
+        myList.display();
+        System.out.println();
+        System.out.println("------------");
+        myList.remove(22);
+        myList.display();
+        System.out.println();
+        System.out.println("------------");
+        myList.removeAllkey(33);
+        myList.display();
     }
 }
